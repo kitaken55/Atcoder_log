fdb1523282ee431486350c728f99017fa239b5cf
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2021-04-26 22:08:23 +0900

    Atcoder tutorial

---
+++ C++/20210426.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//割り算はあとにしたほうが想定通りに動作する
+int main() {
+  cout <<  100 * (100+1) / 2 << endl;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+// int main() {
+//   cout << 0 / 3 << endl;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+// int main() {
+//   cout << 1 + 1 << endl; // 2
+//   cout << 3 - 4 << endl; // -1
+//   cout << 2 * 3 << endl; // 6
+//   cout << 7 / 3 << endl; // 2
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+// int main() {
+// //↓に全角スペース
+// 　cout << "Hello, world!" << endl;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//   cout << 2525 << endl;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 　cout << "a";
+//   cout << "b" << endl;
+//   cout << "c" << "d" << endl;
+// }
