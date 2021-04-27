ff7f68503f4a9a14590944f43101cec1cd00dc8d
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2021-04-27 21:31:08 +0900

    C++ tutorialF - 1.05.実行順序と入力

---
+++ C++/20210427.cc
@@ -0,0 +1,88 @@
+// // ABC問題 - Rating Goal
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int R,G;
+    cin >> R >> G;
+    cout << 2 * G - R << endl;
+}
+
+// // ABC問題 - New Year
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int M;
+//     cin >> M;
+//     cout << 48 - M << endl;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//   int a, b, c;
+//   cin >> a >> b >> c;
+//   cout << a * b * c << endl;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+// int main() {
+//   string text;
+//   double d;
+//   cin >> text;
+//   cin >> d;
+//   cout << text << ", " << d << endl;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int a;
+//   // 変数aで入力を受け取る
+//   cin >> a;
+
+//   cout << a * 10 << endl;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+//   int name;
+
+//   // nameに10を代入
+//   name = 10;
+//   cout << name << endl; // 10
+
+//   // nameに5を代入
+//   name = 5;
+//   cout << name << endl; // 5
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//   int name;
+//   cout << name << endl; //なにが出力されるかわからない
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main(void){
+//     // Your code here!
+//     int name = 10, b = 20;
+    
+//     cout << b << endl;
+//     cout << name << endl;     // 10
+//     cout << name + 2 << endl; // 10 + 2 → 12
+//     cout << name * 3 << endl; // 10 * 3 → 30
+// }
