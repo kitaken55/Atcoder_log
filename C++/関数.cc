918cb0ff5ecda0ec2a6f89654b3fd3f0a218da4f
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2021-05-12 22:05:29 +0900

    practice

--- C++/関数.cc
+++ C++/関数.cc
@@ -3,6 +3,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 整数の入力を受け取って返す関数
+// 引数なし
+int input() {
+  int x;
+  cin >> x;
+  return x;
+}
+
+int main() {
+  int num = input(); // 引数なし
+  cout << num + 5 << endl;
+}
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 // 関数定義
 int my_min(int x, int y) {
 
