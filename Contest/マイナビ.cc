e80262916a04ed09658c4a8648e7fd395176903c
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2021-05-16 19:59:06 +0900

    コンテスト復習

--- Contest/マイナビ.cc
+++ Contest/マイナビ.cc
@@ -1,3 +1,26 @@
+//A ミス内容が「Yes」をYESに。「No」を「NO」と書いてしまっていた。
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> array(3);
+    int i = 0;
+    while(i < 3) {
+     cin >> array.at(i);
+     i++;
+    }
+
+    sort(array.begin(), array.end());
+    
+    if(array.at(0)-array.at(1) == array.at(1)-array.at(2)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
+
+
+//https://atcoder.jp/contests/abc201/editorial/1824 (回答)
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,23 +38,3 @@ int main() {
 
 }
 
-//A
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    vector<int> array(3);
-    int i = 0;
-    while(i < 3) {
-     cin >> array.at(i);
-     i++;
-    }
-
-    sort(array.begin(), array.end());
-    
-    if(array.at(0)-array.at(1) == array.at(1)-array.at(2)) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
-}
