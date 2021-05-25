ff942d5853850e9f97f5d9f1ad5b393a22d6f4da
kitaken55 <MiwaKentoDrums@yahoo.co.jp>
2021-05-25 23:02:42 +0900

    続き

--- C++/第2章/再帰関数.cc
+++ C++/第2章/再帰関数.cc
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 0~0の総和を求める
+int sum0() {
+  return 0;
+}
+// 0~1の総和を求める
+int sum1() {
+  int s = sum0();
+  return s + 1;  // (0~0の総和) + 1 = 1
+}
+// 0~2の総和を求める
+int sum2() {
+  int s = sum1();
+  return s + 2;  // (0~1の総和) + 2 = 3
+}
+// 0~3の総和を求める
+int sum3() {
+  int s = sum2();
+  return s + 3;  // (0~2の総和) + 3 = 6
+}
+
+int main() {
+  cout << sum3() << endl;  // 6
+}
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 int sum(int n) {
   if (n == 0) {
     return 0;
